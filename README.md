08da19acfd5fb3873b618b6216abbc7e571a204c
LarsVandenberghe <lars.vandenberghe@hotmail.be>
2021-06-16 22:19:02 +0200

    Add project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # IOTA-easy-wallet
 a simple python IOTA wallet implementation
+
+
+# use https://faucet.testnet.chrysalis2.com/ to request tokens
---
+++ domain/iota_easy_wallet.py
@@ -0,0 +1,64 @@
+
+import iota_wallet as iw
+import os
+
+def create_account(name, password, node="https://api.hornet-0.testnet.chrysalis2.com") => AccountInterface:
+    temp_account_manager = iw.AccountManager(
+        storage_path='./{}-database'.format(name)
+    )
+    temp_account_manager.set_stronghold_password(password)
+    temp_account_manager.store_mnemonic("Stronghold")
+    client_options = {
+        "nodes": [
+            {
+                "url": node,
+                "auth": None,
+                "disabled": False
+            }
+        ],
+        "local_pow": True
+    }
+    account_initialiser = temp_account_manager.create_account(client_options)
+    account_initialiser.alias(name)
+    account_initialiser.initialise()
+    
+
+def restore_account(file_name, name, password) => AccountInterface:
+    backup_file_path = './backup/{}'.format(file_name)
+    temp_account_manager = iw.AccountManager(
+        storage_path='./{}-database'.format(name)
+    )
+    temp_account_manager.import_accounts(backup_file_path, password)
+    temp_account_manager = None
+    return login(name, password)
+
+def login(name, password) => AccountInterface:
+    return AccountInterface(name, password)
+
+class AccountInterface(object):
+    def __init__(self, name, password):
+        self.__password = password
+        self.__name = name
+        
+        self.__account_manager = iw.AccountManager(
+            storage_path='./{}-database'.format(self.__name)
+        )
+        self.__account_manager.set_stronghold_password(password)
+        self.__account = self.__account_manager.get_account(self.__name)
+        
+    def addressess(self):
+        print(self.__account.addresses())
+        
+    def balance(self):
+        print(self.__account.balance())
+        
+    def backup(self):
+        backup_dir_path = './backup'
+        if not os.path.exists(backup_dir_path):
+            os.makedirs(backup_dir_path)
+        backup_file_path = self.__account_manager.backup(backup_dir_path, self.__password)
+        
+        print(f'Backup path: {backup_file_path}')
+        
+    def get_backup_files(self):
+        return os.listdir('./backup')
